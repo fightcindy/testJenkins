6a3fff2eb553ec4070207ff71e6ef4509710a3dc
fightcindy <biggirlcindy@163.com>
2017-07-12 11:31:35 +0800

    fifth commit src/

---
+++ src/test/java/com/dcc/testJenkins/FistExample.java
@@ -0,0 +1,11 @@
+package com.dcc.testJenkins;
+
+/*
+* 这是第一个例子用来在 github 和 Jenkins 上运行
+*/
+
+public class FistExample {
+	public static void main(String[] args) {
+		System.out.println("Hello Java! Hello Github! Hello Jenkins ");
+	}
+}
